53fb1c10a5501056a077af006c09e0eb861dad4a
Ashley Barcel <ashleyemanon@hotmail.com>
2022-11-12 13:05:37 -0500

    Bookmark Model

--- db/migrate/20221111022556_create_bookmarks.rb
+++ db/migrate/20221111022556_create_bookmarks.rb
@@ -1,8 +1,8 @@
 class CreateBookmarks < ActiveRecord::Migration[7.0]
   def change
     create_table :bookmarks do |t|
-      t.references :user_id, null: false, foreign_key: true
-      t.references :recipe_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :recipe, null: false, foreign_key: true
 
       t.timestamps
     end
