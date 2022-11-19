4e0864f3b340635c7cbc9b92f616f154383e61dc
TaishoIwamoto <h.m.tai.26@gmail.com>
2022-11-19 12:24:36 -0600

    fixed

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,20 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   has_many :reviews
+
+  def avg_score
+    unless self.reviews.empty?
+      reviews.average(:score).round(1)
+    else
+      0.0
+    end
+  end
+
+  def avg_score_percentage
+    unless self.reviews.empty?
+      reviews.average(:score).round(1).to_f*100/5
+    else
+      0.0
+    end
+  end
 end
