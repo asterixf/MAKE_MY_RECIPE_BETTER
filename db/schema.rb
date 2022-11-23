8da1e80e69cc36b6901efdae6ffbe7988415b41c
TaishoIwamoto <h.m.tai.26@gmail.com>
2022-11-22 21:48:30 -0600

    changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_024148) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_020826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_024148) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "recipe_id"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
