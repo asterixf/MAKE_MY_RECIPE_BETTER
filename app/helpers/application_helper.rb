508f4204a74747854be5151456910feed8fca100
TaishoIwamoto <h.m.tai.26@gmail.com>
2022-12-01 22:09:39 -0600

    done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def decorate_ingredients(ingredients)
     return ingredients.split(",").map {|ingredient| ingredient.remove(",")}
   end
+
+  def decorate_directions(directions)
+    return directions.first.step.split("/").map {|step| step.remove("/")}
+  end
 end
