f1777e75afc63c268a02ba2fa4967dd527201bd0
fernandozuniga <carlosfernando.zh@gmail.com>
2022-12-08 21:22:56 -0600

    created my recipes page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,8 @@ class PagesController < ApplicationController
   def home
     @recipes = Recipe.includes(:reviews, photo_attachment: :blob).all.sample(12)
   end
+
+  def show
+    @recipes = Recipe.where(user:current_user)
+  end
 end
